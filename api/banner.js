fe0b61a8a02599974c0a1ab5032266cdc5874668
公来 周 <zhougonglai@dingtalk.com>
2019-10-11 10:49:53 +0800

    type: update

--- api/banner.js
+++ api/banner.js
@@ -3,7 +3,9 @@ const Mock = require('mockjs');
 module.exports = (req, res) => {
 	res.json(
 		Mock.mock({
-			'data|4-8': ['@image(822x220, @color, #FFF, png, @csentence(3, 7))'],
+			'data|4-8': [
+				'https://imgslim.geekpark.net/uploads/image/file/57/43/5743dbbad9f70f1ed9fc42b4c6ae6ea9.jpg',
+			],
 			status: true,
 		}),
 	);
